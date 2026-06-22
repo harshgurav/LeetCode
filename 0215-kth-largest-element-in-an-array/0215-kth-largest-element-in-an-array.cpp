26a44994ce45b4cbed4ce5a78f51e0a7309d7105
Harsh Gurav <harshgurav3021@gmail.com>
2026-06-22 15:21:07 +0530

    Time: 28 ms (68.55%), Space: 75.4 MB (6.15%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+class Heap{
+    public:
+    int size;
+    vector<int>heap;
+    Heap(vector<int> nums){
+        size = 0;
+    }
+void insert_node(int val){
+    heap.push_back(val);
+    int i = size;
+    size++;
+    
+    // We use i > 0 to ensure we don't go out of bounds (below the root).
+    // The parent of index 'i' in a 0-indexed array is (i - 1) / 2.
+    while(i > 0 && heap[(i - 1) / 2] > heap[i]){
+        swap(heap[(i - 1) / 2], heap[i]);
+        i = (i - 1) / 2;
+    }
+}
+    void heapify(int i){
+        int leftChild = 2*i + 1;
+        int rightChild = 2*i + 2;
+        int smallest = i;
+        if(leftChild < size && heap[leftChild] < heap[smallest]){
+            smallest = leftChild;
+        }
+        if(rightChild < size && heap[rightChild] < heap[smallest]){
+            smallest = rightChild;
+        }
+        if(smallest != i){
+            swap(heap[smallest], heap[i]);
+            heapify(smallest);
+        }
+    }
+    void delete_root(){
+        swap(heap[0], heap[size-1]);
+        heap.pop_back();
+        size--;
+        heapify(0);
+    }
+};
+
+    int findKthLargest(vector<int>& nums, int k) {
+        Heap heap1(nums);
+        for(int i =0; i<nums.size(); i++){
+            if(heap1.size<k){
+                heap1.insert_node(nums[i]);
+            }
+            else if(nums[i] > heap1.heap[0]){
+                heap1.delete_root();
+                heap1.insert_node(nums[i]);
+            }
+            else{continue;}
+        }
+    
+    
+    return heap1.heap[0];
+    }
+};
