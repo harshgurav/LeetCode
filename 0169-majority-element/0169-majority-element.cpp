dbd820d3a6e0e2345285058ff057126621a083ad
Harsh Gurav <harshgurav3021@gmail.com>
2026-07-18 17:16:03 +0530

    Time: 1 ms (34.81%), Space: 28.2 MB (69.24%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mpp;
+        for(int i = 0; i < nums.size(); i++) {
+            mpp[nums[i]]++;
+
+            if(mpp[nums[i]] > (nums.size() / 2)){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
